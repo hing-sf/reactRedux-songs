ce0dbcb179499b249445830b24e0a64ed8e7d23a
tang07 <htang@levi.com>
2018-11-28 13:07:49 -0800

    completed project

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import SongList from './SongList';
+import SongDetails from './SongDetails';
+
+import { selectSong } from '../actions';
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className='ui container grid'>
+			<div className='ui row'>
+				<div className='column eight wide'>
+					<SongList />
+				</div>
+				<div className='colmun eight wide'>
+					<SongDetails />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './components/App';
+import reducers from './reducers';
 
-export default class index extends Component {
-	render() {
-		return <div />;
-	}
-}
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+	<Provider store={createStore(reducers)}>
+		<App />
+	</Provider>,
+	document.querySelector('#root')
+);
